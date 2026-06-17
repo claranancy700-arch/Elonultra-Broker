80d2b5c5e3463163755d6ecb4e18e57712740a20
your-username <your-email@example.com>
2026-06-17 07:31:00 -0400

    withdrawal changes

--- frontend/src/components/pages/Withdrawals/WithdrawalsPage.jsx
+++ frontend/src/components/pages/Withdrawals/WithdrawalsPage.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import './WithdrawalsPage.css';
 import API from '../../../services/api';
 
-const MIN_WITHDRAWAL_FEE_RATE = 0.00045;
-const MAX_WITHDRAWAL_FEE_RATE = 0.0005;
+const MIN_WITHDRAWAL_FEE_RATE = 0.0055;
+const MAX_WITHDRAWAL_FEE_RATE = 0.008;
 
 export const WithdrawalsPage = () => {
   const [crypto, setCrypto] = useState('BTC');
